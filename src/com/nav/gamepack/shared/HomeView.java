53806a00fcfa33dbd789bb0d0dc3de6a3b26c8fb
Naveed Ahmad <naveed.ahmad@7vals.com>
2012-01-01 17:41:51 +0500

    add home view

---
+++ src/com/nav/gamepack/shared/HomeView.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.nav.gamepack.shared;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * @author naveed
+ *
+ */
+public class HomeView extends View {
+  
+	public Bitmap backgroungImage;
+	/**
+	 * @param context
+	 */
+	public HomeView(Context context) {
+		super(context);
+		 
+		// TODO Auto-generated constructor stub
+	}
+  
+	/* (non-Javadoc)
+	 * @see android.view.View#onDraw(android.graphics.Canvas)
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+}
