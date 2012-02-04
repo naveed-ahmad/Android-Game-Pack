ee174502a9b626dadf9e38f1600634e80464930e
Naveed Ahmad <naveed.ahmad@7vals.com>
2012-02-05 00:18:23 +0500

    add anim resources and jigsaw setting view

--- gen/com/nav/gamepack/R.java
+++ gen/com/nav/gamepack/R.java
@@ -101,25 +101,41 @@ public final class R {
         public static final int two=0x7f020053;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int group1=0x7f070007;
-        public static final int imageView1=0x7f070005;
-        public static final int imageViewCloudMediumAnimated=0x7f070004;
-        public static final int imageViewCloudSmallAnimated=0x7f070003;
-        public static final int imageViewMyJigsaw=0x7f070006;
-        public static final int jigsaw=0x7f070008;
-        public static final int jigsawBoard=0x7f070000;
+        public static final int button1=0x7f070004;
+        public static final int button2=0x7f070005;
+        public static final int group1=0x7f07000d;
+        public static final int imageView1=0x7f070008;
+        public static final int imageViewCloudMediumAnimated=0x7f070007;
+        public static final int imageViewCloudSmallAnimated=0x7f070006;
+        public static final int imageViewMyJigsaw=0x7f070009;
+        public static final int jigsaw=0x7f07000e;
+        public static final int jigsawBoard=0x7f070003;
+        public static final int menu_back=0x7f07000c;
+        public static final int menu_jigsaw_setting=0x7f07000a;
+        public static final int menu_save_game=0x7f07000b;
+        public static final int seekBarJigsawBoardRows=0x7f070001;
+        public static final int txtViewJigsawBoardRowsCount=0x7f070002;
+        public static final int txtViewRowsMsg=0x7f070000;
     }
     public static final class layout {
-        public static final int jigsawboard=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int jigsaw_setting=0x7f030000;
+        public static final int jigsawboard=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int jigsaw_activity_menu=0x7f060000;
+        public static final int main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f050001;
+        public static final int back=0x7f050008;
         public static final int hello=0x7f050000;
+        public static final int jigsaw_baord_default_columns=0x7f050005;
+        public static final int jigsaw_baord_default_rows=0x7f050004;
+        public static final int jigsaw_baord_max_rows_and_columns=0x7f050006;
+        public static final int jigwas_setting_column_lbl=0x7f050003;
+        public static final int jigwas_setting_rows_lbl=0x7f050002;
+        public static final int save_game=0x7f050009;
+        public static final int setting_lbl=0x7f050007;
     }
 }
--- src/com/nav/gamepack/puzzle/jigsaw/JigsawActivity.java
+++ src/com/nav/gamepack/puzzle/jigsaw/JigsawActivity.java
@@ -2,7 +2,19 @@ package com.nav.gamepack.puzzle.jigsaw;
 
 import com.nav.gamepack.R;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * @author Naveed Ahmad
@@ -10,9 +22,59 @@ import android.os.Bundle;
  */
 
 public class JigsawActivity extends Activity {
+	AlertDialog settingDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.jigsawboard);
+
+		setContentView(R.layout.main);
 	}
+
+	public void showSettingDialog() {
+		if (settingDialog == null) {
+
+			LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			final View signinView = layoutInflater.inflate(R.layout.jigsaw_setting, null);
+			settingDialog = new AlertDialog.Builder(this).setView(signinView).setTitle("Login Required").setPositiveButton("Signin", null).create();
+			settingDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+				public void onShow(DialogInterface dialog) {
+					Button b = settingDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+					b.setOnClickListener(new View.OnClickListener() {
+						public void onClick(View view) {
+
+						}
+					});
+				}
+			});
+		}
+		settingDialog.show();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = new MenuInflater(getApplicationContext());
+		menuInflater.inflate(R.menu.jigsaw_activity_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean handleMenuCick = true;
+		if (item.getItemId() == R.id.menu_jigsaw_setting) {
+			showSettingDialog();
+
+		} else if (item.getItemId() == R.id.menu_back) {
+
+		} else if (item.getItemId() == R.id.menu_save_game) {
+
+		}
+
+		else
+			handleMenuCick = false;
+
+		return handleMenuCick;
+	}
+
 }
